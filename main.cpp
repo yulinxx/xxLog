505006e1497edeb32dd7eb960ee5618ac1ffc8aa
yulinxx <yulinxx@gmail.com>
2022-09-23 23:14:47 +0800

    使用日志测试

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main()
     int i = 0;
 
     char szData[128] = "此文件包含 main 函数。程序执行将在此处开始并结束";
-    for (int i = 0; i < 1000; i++)
+
+    for (int i = 0; i < 100; i++)
     {
         RDLOG_TRACE(szData << i);
         RDLOG_DEBUG(szData << i);
@@ -15,15 +16,21 @@ int main()
         RDLOG_ERROR(szData << i);
         RDLOG_FATAL(szData << i);
 
+        std::string strInfo = "abc中文+-*/";
+        RDLOG_INFO(strInfo);
+
+        const char* pCh = "xxx 1123j另文 朝右两岸三地 f32111\n\naaaabccc";
+        RDLOG_INFO(pCh);
+
         if (i == 30)
         {
-            RDLOG_CLOSE();
+            RDLOG_OFF();
         }
 
         if (i == 80)
             RDLOG_REC_ERROR();
 
-        if (i == 90)
+        if (i == 93)
             RDLOG_ON()
     }
 
